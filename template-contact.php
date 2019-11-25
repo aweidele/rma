54b6f5806409f22dd0a89824667b099da5cc302d
Aaron Weidele <aweidele@r2integrated.com>
2019-11-24 22:26:51 -0500

    custom map

--- template-contact.php
+++ template-contact.php
@@ -44,7 +44,7 @@
         </ul>
       </div>
       <div>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.343919550055!2d-73.99655208459382!3d40.75445987932725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259ad78db4755%3A0x86c828979ef6d76a!2s347%20W%2036th%20St%2C%20New%20York%2C%20NY%2010018!5e0!3m2!1sen!2sus!4v1574217628149!5m2!1sen!2sus" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+        <iframe src="https://www.google.com/maps/d/embed?mid=1LIPP_TQtZLWzcCjLvOP6abjqYrXQgCYE" width="640" height="480"></iframe>
       </div>
     </div>
   </div>
