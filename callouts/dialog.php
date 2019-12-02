51c273b68577c0de247ee93edbcfc0ac22506014
Aaron Weidele <aweidele@r2integrated.com>
2019-12-02 09:57:03 -0500

    Fix the close button on dialog

--- callouts/dialog.php
+++ callouts/dialog.php
@@ -1,6 +1,7 @@
 <div id="dialog" class="dialog">
   <div class="dialog-inner">
     <button>
+      <span class="ic ic-close-box"></span>
       <span class="sr-only">Close</span>
     </button>
     <div class="dialog-content"></div>
