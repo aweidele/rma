6dd42f615afb6bf3682953459bf5fb044feef2f8
Aaron Weidele <aweidele@r2integrated.com>
2020-02-02 21:49:27 -0500

    qweqwe

--- archive-projects.php
+++ archive-projects.php
@@ -71,7 +71,7 @@
         <div class="article-list listing-table">
           <table id="projectTable">
             <thead>
-              <th tabindex="0" aria-sort="ascending"><div>Project</div></th>
+              <th tabindex="0"><div>Project</div></th>
               <th tabindex="0"><div>Client</div></th>
               <th tabindex="0" data-sort-method="number" aria-sort="descending" data-sort-default><div>Date</div></th>
               <th class="sector" tabindex="-1"><div>Sector</div></th>
